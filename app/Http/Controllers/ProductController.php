577042f99f254cc669ae11b47c903cf4d8edce8d
Winxx0102 <olowixtovar@gmail.com>
2026-02-02 18:11:25 -0400

    Add controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,4 +15,32 @@ class ProductController extends Controller
         $productos=Producto::all();
         return view('index',compact('productos'));
     }
+    /** * Show the form for creating a new resource. */
+    public function create() {
+        //
+         }
+          /** * Store a newly created resource in storage. */
+          public function store(Request $request) {
+            //
+           }
+           /** * Display the specified resource. */
+
+            public function show(Producto $producto) {
+                //
+             }
+             /** * Show the form for editing the specified resource. */
+
+             public function edit(Producto $producto) {
+                 //
+             }
+             /** * Update the specified resource in storage. */
+
+             public function update(Request $request, Producto $producto) {
+                 //
+             }
+             /** * Remove the specified resource from storage. */
+
+             public function destroy(Producto $producto) {
+                //
+             }
 }
